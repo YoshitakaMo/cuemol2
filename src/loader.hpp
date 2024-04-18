d52f6f65c26fed9e6c87f7700090993be674fa8f
Ryuichiro Ishitani <you@example.com>
2024-04-18 16:39:47 +0900

    .

--- src/loader.hpp
+++ src/loader.hpp
@@ -2,13 +2,16 @@
 
 #ifdef WIN32
 // for Windows
-#ifdef LIBCUEMOL_EXPORTS
-#define LIBCUEMOL_API __declspec(dllexport)
-#elif defined(LINK_SHARED)
-#define LIBCUEMOL_API __declspec(dllimport)
+#  ifdef LIBCUEMOL_EXPORTS
+#    define LIBCUEMOL_API __declspec(dllexport)
+#  elif defined(LINK_SHARED)
+#    define LIBCUEMOL_API __declspec(dllimport)
+#  else
+#    define LIBCUEMOL_API
+#  endif
 #else
-#define LIBCUEMOL_API
-#endif
+// other platforms
+#  define LIBCUEMOL_API
 #endif
 
 #include <common.h>
