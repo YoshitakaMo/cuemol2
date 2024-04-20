30c69d043cc487c74d28064ea333ace0984d2a59
ishitani <ishitani@users.sourceforge.net>
2024-04-20 19:13:47 +0900

    fix boost path

--- build_scripts/build_cuetty_posix.sh
+++ build_scripts/build_cuetty_posix.sh
@@ -3,6 +3,7 @@ set -eux
 
 BASEDIR=$1
 ARCH=$2
+BOOST_VER=boost_1_84_0
 
 # Install location
 INST_PATH=$BASEDIR/cuemol2
@@ -20,11 +21,12 @@ ls -la $BASEDIR
 cmake -S ${GITHUB_WORKSPACE}/cli -B $BUILD_DIR \
       -DCMAKE_INSTALL_PREFIX=$INST_PATH \
       -DCMAKE_PREFIX_PATH=$BASEDIR \
-      -DBoost_ROOT=$BASEDIR/boost_1_84/ \
+      -DBoost_ROOT=$BASEDIR/$BOOST_VER/ \
       -DLIBCUEMOL2_ROOT=$BASEDIR\cuemol2 \
       -DCMAKE_BUILD_TYPE=$BUILD_TYPE
 
 make -j 8
 make install
 
-cp $BASEDIR/boost_1_84/lib/lib* $BASEDIR/cuemol2/lib/
+ls -la $BASEDIR/$BOOST_VER/lib/lib*
+cp $BASEDIR/$BOOST_VER/lib/lib* $BASEDIR/cuemol2/lib/
