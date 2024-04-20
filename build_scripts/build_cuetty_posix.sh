2337eb54bb73e3c3ba137d5bc7afaeb34c82f49c
ishitani <ishitani@users.sourceforge.net>
2024-04-20 11:02:16 +0900

    add cuetty build

---
+++ build_scripts/build_cuetty_posix.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -eux
+
+BASEDIR=$1
+ARCH=$2
+
+# Install location
+INST_PATH=$BASEDIR/cuemol2
+
+# Build
+BUILD_DIR=$BASEDIR/build_cuetty
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+
+# BUILD_TYPE=Debug
+BUILD_TYPE=Release
+
+ls -la $BASEDIR
+
+cmake -S ${GITHUB_WORKSPACE} -B $BUILD_DIR \
+      -DCMAKE_INSTALL_PREFIX=$INST_PATH \
+      -DCMAKE_PREFIX_PATH=$BASEDIR \
+      -DBoost_ROOT=$BASEDIR/boost_1_84/ \
+      -DLIBCUEMOL2_ROOT=$BASEDIR\cuemol2 \
+      -DCMAKE_BUILD_TYPE=$BUILD_TYPE
+
+make -j 8
+make install
+
