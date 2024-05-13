c93d1e66e4f2f4f28ca73628a270b976ccc943b8
Ryuichiro Ishitani <you@example.com>
2024-05-13 15:11:04 +0900

    fix archive type

--- build_scripts/download_deplibs/run_windows.sh
+++ build_scripts/download_deplibs/run_windows.sh
@@ -10,13 +10,12 @@ mkdir -p $TMPDIR
 cd $TMPDIR
 
 # Retrieve deplibs binary
-DEPLIBS_TGZ=deplibs_${RUNNER_OS}_${RUNNER_ARCH}.tar.bz2
+DEPLIBS_TGZ=deplibs_${RUNNER_OS}_${RUNNER_ARCH}.tar.gz
 DEPLIBS_VERSION=v0.0.2
 DEPLIBS_URL=https://github.com/CueMol/build_prerequisites/releases/download
 wget --progress=dot:mega -c \
      $DEPLIBS_URL/$DEPLIBS_VERSION/$DEPLIBS_TGZ
-# xattr -cr $DEPLIBS_TGZ
-tar xjf $DEPLIBS_TGZ
 
+tar xzf $DEPLIBS_TGZ
 mv proj64_deplibs $BASEDIR/
 
