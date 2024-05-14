706c60b9d1a0ffaab0672ebc43b51b302b9485df
ishitani <ishitani@users.sourceforge.net>
2024-05-14 17:02:28 +0900

    update workflows

--- build_scripts/download_deplibs/run_windows.sh
+++ build_scripts/download_deplibs/run_windows.sh
@@ -17,5 +17,5 @@ wget --progress=dot:mega -c \
      $DEPLIBS_URL/$DEPLIBS_VERSION/$DEPLIBS_TGZ
 
 tar xzf $DEPLIBS_TGZ
+mkdir -p $BASEDIR/proj64_deplibs
 mv -v proj64_deplibs/* $BASEDIR/proj64_deplibs
-
