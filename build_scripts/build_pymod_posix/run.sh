60a40f4219c8c435b84a1902eb9cea788a2c392c
Ryuichiro Ishitani <r.ishitani@tmd.ac.jp>
2024-09-03 17:25:44 +0900

    add pymod build script

---
+++ build_scripts/build_pymod_posix/run.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# build script for pymod in posix
+# usage: run.sh deplibs_dir
+#
+
+set -eux
+
+BASEDIR=$1
+REPOS_DIR=$(cd $(dirname $0)/../..; pwd)
+WORKSPACE=${GITHUB_WORKSPACE:-$REPOS_DIR}
+
+BOOST_VER=boost_1_84_0
+
+# Install location
+INST_PATH=$BASEDIR/cuemol2
+
+cd $WORKSPACE/pymod
+pip3 install \
+     --config-settings=cmake.define.LIBCUEMOL2_ROOT=$INST_PATH \
+     -v . 
+
+# Copy dependent libs (boost)
+ls -la $BASEDIR/$BOOST_VER/lib/lib*
+cp $BASEDIR/$BOOST_VER/lib/lib* $BASEDIR/cuemol2/lib/
