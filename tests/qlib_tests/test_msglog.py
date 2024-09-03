4debb4f61258d2d6ee54674fdf2627ba7869ca3f
ishitani <ishitani@users.sourceforge.net>
2024-09-03 19:18:16 +0900

    add bytearray pytest

--- tests/qlib_tests/test_msglog.py
+++ tests/qlib_tests/test_msglog.py
@@ -5,22 +5,41 @@ def test_qlib_msglog():
     svc = cuemol.getService("MsgLog")
     assert svc
 
-    svc.write("test_str")
+def test_qlib_msglog_write(capfd):
+    svc = cuemol.getService("MsgLog")
+    svc.write("test_str ")
 
     svc.writeln("test_str")
     
-    s = svc.getAccumMsg()
-    print(f"{s=}")
+    captured = capfd.readouterr()
+    print(f"XXX {captured.out=}")
+    print(f"XXX {captured.err=}")
+    assert "test_str test_str" in captured.out
+    # assert False
 
+def test_qlib_msglog_write(capfd):
+    svc = cuemol.getService("MsgLog")
+    svc.removeAccumMsg()
+    svc.writeln("XXXXXXXXXX")
+    s = svc.getAccumMsg()
+
+    # msg should not be accumlated
+    assert s == ""
     
 def test_qlib_msglog_redir(tmp_path):
     print(tmp_path)
     svc = cuemol.getService("MsgLog")
     log_file = tmp_path / "log.txt"
     svc.setFileRedirPath(str(log_file))
-    svc.writeln("test_str")
+    svc.writeln("YYYYYYYYYY")
     chk = svc.getFileRedirPath() 
     print(f"{chk=}")
     assert chk.endswith("log.txt")
     svc.setFileRedirPath("")
-    assert False
+    svc.writeln("XXXXXXXXXX")
+
+    with log_file.open() as f:
+        dat = f.read()
+    assert "YYYYYYYYYY" in dat
+    assert "XXXXXXXXXX" not in dat
+    # print(dat)
