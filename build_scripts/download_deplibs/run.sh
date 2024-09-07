a3268fbcb7f2101c1cb477192cb0637e83bfa82a
ishitani <ishitani@users.sourceforge.net>
2024-09-07 11:37:29 +0900

    update deplibs

--- build_scripts/download_deplibs/run.sh
+++ build_scripts/download_deplibs/run.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 set -eux
 
+DEPLIBS_VERSION=v0.0.4
+
 BASEDIR=$1
 RUNNER_OS=$2
 RUNNER_ARCH=$3
-TMPDIR=$BASEDIR/tmp
 
+##########
+
+TMPDIR=$BASEDIR/tmp
 mkdir -p $TMPDIR
 cd $TMPDIR
 
 # Retrieve deplibs binary
 DEPLIBS_TGZ=deplibs_${RUNNER_OS}_${RUNNER_ARCH}.tar.bz2
-DEPLIBS_VERSION=v0.0.3
-# DEPLIBS_VERSION=v0.0.1
+
 wget --progress=dot:mega -c \
      https://github.com/CueMol/build_scripts/releases/download/$DEPLIBS_VERSION/$DEPLIBS_TGZ
 # xattr -cr $DEPLIBS_TGZ
 tar xjf $DEPLIBS_TGZ
 
 mv target/* $BASEDIR/
-# mv builds/* $BASEDIR/
